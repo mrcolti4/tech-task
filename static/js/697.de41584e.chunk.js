01474c12a0ba04f21513ba1ff83db708c2d576f9
mrcolti4 <68332032+mrcolti4@users.noreply.github.com>
2023-12-03 14:26:51 +0000

    Deploying to gh-pages from @ mrcolti4/tech-task@55f52f35a1b8eb7d1d009f1231a3d40bc940e284 🚀

---
+++ static/js/697.de41584e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktech_task=self.webpackChunktech_task||[]).push([[697],{5697:function(e,t,s){s.r(t);s(2791);var a=s(184);t.default=function(){return(0,a.jsx)("div",{children:"FavoritesPage"})}}}]);
+//# sourceMappingURL=697.de41584e.chunk.js.map
